930323f5f02aec18bd3534d132d5c8e18e0c7d35
virajmehta100 <viraj1messi10@gmail.com>
2022-06-20 12:54:54 +0530

    stying and timing

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,14 +6,17 @@ export class NewsItem extends Component {
         width: "18rem"
     }*/
   render() {
-     let {title,description,imageUrl,newsUrl}=this.props;
+     let {title,description,imageUrl,newsUrl, author, date, source}=this.props;
     return (
       <div className="my-3">
           <div className="card" /*style={this.myStyle}*/>
+          <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left:'90%',zIndex:'1'}}>
+              {source}</span>
             <img src={imageUrl} className="card-img-top" alt="..."/>
         <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
+            <p className="card-text"><small className="text-muted">By {!author?"Unknown":author} on {new Date(date).toGMTString()}</small></p>
             <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
         </div>
         </div>
